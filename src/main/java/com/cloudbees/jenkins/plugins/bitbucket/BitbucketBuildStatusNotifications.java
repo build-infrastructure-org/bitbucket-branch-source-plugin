7bba0166ed5a835623a17a3d46c091bffa983bed
Rajeshwari Subramaniam <Subramaniam.Rajeshwari@in.bosch.com>
2022-09-09 13:41:30 +0530

    temp fix: mark build to failure when NOT_BUILT permanent fix: https://github.com/jenkinsci/workflow-job-plugin/pull/294

--- src/main/java/com/cloudbees/jenkins/plugins/bitbucket/BitbucketBuildStatusNotifications.java
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/BitbucketBuildStatusNotifications.java
@@ -146,10 +146,6 @@ public class BitbucketBuildStatusNotifications {
         } else if (Result.FAILURE.equals(result)) {
             statusDescription = StringUtils.defaultIfBlank(buildDescription, "There was a failure building this commit.");
             state = FAILED_STATE;
-        } else if (Result.NOT_BUILT.equals(result)) {
-            // Bitbucket Cloud and Server support different build states.
-            state = (bitbucket instanceof BitbucketCloudApiClient) ? STOPPED_STATE : SUCCESSFUL_STATE;
-            statusDescription = StringUtils.defaultIfBlank(buildDescription, "This commit was not built (probably the build was skipped)");
         } else if (result != null) { // ABORTED etc.
             statusDescription = StringUtils.defaultIfBlank(buildDescription, "Something is wrong with the build of this commit.");
             state = FAILED_STATE;
